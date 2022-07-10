8df27f9f1a4f8e421e734b157f35ea6bc381548c
Sean M <sean.maddox89@yahoo.com>
2022-07-10 16:42:40 -0700

    running random number test

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,6 +1,15 @@
-
-const checkIfEqual = require ('../lib/random.js');
+// Equal Test
+const checkIfEqual = require('../lib/random.js');
 
 test('checks if 10 is equal to 10', () => {
     expect(checkIfEqual(10,10)).toBe(true);
 });
+
+
+// Random Number Test
+const randomNumber = require('../lib/random.js');
+
+test('gets random number between 1 and 10', () => {
+    expect(randomNumber()).toBeGreaterThanOrEqual(1);
+    expect(randomNumber()).toBeLessThanOrEqual(10);
+})
